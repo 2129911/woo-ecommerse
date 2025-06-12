d1759a8d4fddf91382b329a6294ad07d159d0f68
WEB DEV <deepanshu4788@gmail.com>
2025-06-12 16:08:55 +0530

    add url

--- src/app/cart/page.tsx
+++ src/app/cart/page.tsx
@@ -8,7 +8,7 @@ export default function CartPage() {
   const [cartItems, setCartItems] = useState([]);
   const [callgetCart, setcallgetCart] = useState(false);
 
-  const endpoint = process.env.NEXT_PUBLIC_END_POINT;
+  const endpoint:any = "https://wordpress-1347810-5596954.cloudwaysapps.com/graphql1";
 // ---------------------fetch-cart---------------------------------
   const fetchCartData = async () => {
     const storedSession = getSession();   
